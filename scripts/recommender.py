6155a010a7bc85a50c3fc0ef4193c831e642f254
chaithanya <Chaithanyachandran062@gmail.com>
2026-06-10 20:16:58 +0400

    Day 6: Advanced Analytics and Risk Metrics

---
+++ scripts/recommender.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+# Load data
+scheme = pd.read_csv(
+    "../data/processed/scheme_performance_clean.csv"
+)
+
+print("\nAvailable Risk Levels:")
+print(
+    scheme['risk_grade']
+    .dropna()
+    .unique()
+)
+
+risk = input(
+    "\nEnter Risk Level: "
+)
+
+# Filter funds
+result = scheme[
+    scheme['risk_grade'] == risk
+]
+
+# Sort by Sharpe Ratio
+result = result.sort_values(
+    'sharpe_ratio',
+    ascending=False
+)
+
+# Top 3 recommendations
+top3 = result[
+    [
+        'scheme_name',
+        'risk_grade',
+        'sharpe_ratio'
+    ]
+].head(3)
+
+print("\nTop 3 Recommended Funds:\n")
+print(top3)
+
+# Save report
+top3.to_csv(
+    "top3_recommendations.csv",
+    index=False
+)
+
+print("\nRecommendations saved!")
